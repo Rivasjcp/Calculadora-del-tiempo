ee5f1797f828985ecc70748a12cc71f243b49321
rivasjcp <jcrivas1998@gmail.com>
2022-08-27 22:42:17 +0200

    EmpezandoSuma

--- master.js
+++ master.js
@@ -1,24 +1,61 @@
+/* Realizar una calculadora que permite restar y sumar horas, minutos y segundos
+
+- Como primer paso se debe establecer toda la parte visual basica en hyml y css y crear los id, los input y las etiquetas para que se escriban las acciones establecidas
+- Luego escribir los const que estableceran la relacion de los id y las querySelector o clases
+- Primero el usuario escribira las horas, minutos y segundos por defecto y se comprobara por medio de un condifcional if si los valores suministrados en segundos y minutos estan entre 0 y 59
+- Luego de tomar los datos se escribiran y arrojaran en pantalla
+
+Nota a investigar como sumar numeros y que se transformen en horas minutos y segundos
+
+- Si tengo 70 segundos debo tomar un condicional donde tenga 
+*/
+
+
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
 const horas = document.getElementById("horas");
 const escribeUsuario = document.querySelector(".tiempoUsuario");
 const valoresUsuario = document.querySelector(".botonResult")
 
-valoresUsuario.addEventListener('click',Usuario)
-
-function Usuario(){
-    if(minutos.value >= 0 && minutos.value <= 59,segundos.value >= 0 && segundos.value <= 59){
 let laHora = horas.value;
 let losMinutos = minutos.value;
 let losSegundos = segundos.value;
-if(minutos.value > 9,segundos.value > 9){
-escribeUsuario.textContent = `${laHora}:${losMinutos}:${losSegundos}`;
-}else{
-    escribeUsuario.textContent = `${laHora}:0${losMinutos}:0${losSegundos}`;
+
+valoresUsuario.addEventListener('click',Usuario)
+
+function Usuario(){
+    if(minutos.value >= 0 && minutos.value <= 59){
+        if(segundos.value >= 0 && segundos.value <= 59){
+            let laHora = horas.value;
+            let losMinutos = minutos.value;
+            let losSegundos = segundos.value;
+
+            laHora = (laHora < 10)? "0" + laHora : laHora;
+            losMinutos = (losMinutos < 10)? "0" + losMinutos : losMinutos;
+            losSegundos = (losSegundos < 10)? "0" + losSegundos : losSegundos;
+
+            escribeUsuario.textContent = `Hora Introducida: ${laHora}:${losMinutos}:${losSegundos}`;
+        }else{
+            alert("Debe introducir datos entre 0 y 59 minutos y segundos")
+            minutos.value = ""
+            segundos.value = ""
+            escribeUsuario.textContent = ""
+        }
+    }else{
+        alert("Debe introducir datos entre 0 y 59 minutos y segundos")
+        minutos.value = ""
+        segundos.value = ""
+        escribeUsuario.textContent = ""
+    }
 }
-}else{
-    alert("Debe introducir datos entre 0 y 59 minutos y segundos")
-    minutos.value = ""
-    segundos.value = ""
-}
-}
+
+
+
+
+
+//Probando Date
+
+var fecha = new Date();
+var sumarsesion = 5;
+console.log(fecha.getMinutes() + ":" + (fecha.setMinutes(fecha.getMinutes() + sumarsesion) && fecha.getMinutes()));
+
